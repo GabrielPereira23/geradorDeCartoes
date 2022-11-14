4db0250cdc1f054651c5c90ee8119cb8cf155ac0
GabrielPereira23 <gabrielgoulartpereira23@gmail.com>
2022-11-14 19:14:48 -0300

    Bloqueio e desbloqueio do botão

--- js/main.js
+++ js/main.js
@@ -1,13 +1,23 @@
 /* -- Objeto com os itens do DOM -- */
 const dom = {
-  select: document.querySelector('.j-select'),
-}
+  input: document.querySelector(".j-nameInput"),
+  select: document.querySelector(".j-select"),
+  genButton: document.querySelector(".j-genBtn"),
+};
 
 /* -- Preenche o seletor com os modelos -- */
 patterns.forEach((pattern, index) => {
-  const option = document.createElement('option');
+  const option = document.createElement("option");
   option.value = index;
   option.innerText = pattern.name;
   dom.select.appendChild(option);
 });
 
+/* -- Bloqueio e desbloqueio do botão -- */
+dom.input.addEventListener("input", (e) => {
+  if (e.currentTarget.value === "") {
+    dom.genButton.classList.remove("active");
+  } else {
+    dom.genButton.classList.add("active");
+  }
+});
