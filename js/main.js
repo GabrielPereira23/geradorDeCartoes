dd6ce6deab54abde3e11aa22e106b48bceb24536
GabrielPereira23 <gabrielgoulartpereira23@gmail.com>
2022-11-14 21:43:59 -0300

    Geração do cartão e download

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 /* -- Objeto com os itens do DOM -- */
 const dom = {
   backButton: document.querySelector(".j-backBtn"),
+  downloadButton: document.querySelector(".j-downBtn"),
   modal: document.querySelector(".j-modal"),
+  image: document.querySelector(".j-img"),
   input: document.querySelector(".j-nameInput"),
   select: document.querySelector(".j-select"),
   genButton: document.querySelector(".j-genBtn"),
@@ -20,15 +22,6 @@ dom.backButton.addEventListener("click", () =>
   dom.modal.classList.toggle("active")
 );
 
-/* -- Botão que gera a imagem --*/
-let enableButton = false;
-dom.genButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  if (enableButton) {
-    dom.modal.classList.toggle("active");
-  }
-});
-
 /* -- Bloqueio e desbloqueio do botão -- */
 dom.input.addEventListener("input", (e) => {
   if (e.currentTarget.value === "") {
@@ -40,8 +33,31 @@ dom.input.addEventListener("input", (e) => {
   }
 });
 
+/* -- Botão que gera a imagem --*/
+let enableButton = false;
+dom.genButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (enableButton) {
+    genCard(dom.input.value, dom.select.value);
+  }
+});
+
 /* -- Função que gera o cartão -- */
-function genCard(name, pattern, callback) {
+function genCard(name, patternIndex) {
+  genImg(name, patterns[patternIndex], (img) => {
+    dom.image.src = img;
+    dom.modal.classList.toggle("active");
+    const link = document.createElement("a");
+    link.href = img.replace("image/jpeg", "image/octet-stream");
+    link.download = "cartão.jpeg";
+    dom.downloadButton.addEventListener("click", () => {
+      link.click();
+    });
+  });
+}
+
+/* -- Função que gera a imagem do cartão -- */
+function genImg(name, pattern, callback) {
   const canvas = document.createElement("canvas");
   const ctx = canvas.getContext("2d");
   const { width, height, x, y, imgFile, font, color, quality } = pattern;
@@ -50,14 +66,10 @@ function genCard(name, pattern, callback) {
   const image = new Image();
   image.src = imgFile;
   image.onload = () => {
-    ctx.drawImage(image, 0, 0);
+    ctx.drawImage(image, 0, 0, width, height);
     ctx.fillStyle = color;
     ctx.font = font;
     ctx.fillText(name, x, y);
-    callback(
-      canvas
-        .toDataURL("image/jpeg", quality)
-        .replace("image/jpg", "image/octet-stream")
-    );
+    callback(canvas.toDataURL("image/jpeg", quality));
   };
 }
--- js/patterns.js
+++ js/patterns.js
@@ -1,24 +1,9 @@
-const patterns = [
-  {
-    name: "Natal",
-    width: 1080,
-    height: 1080,
-    x: 100,
-    y: 100,
-    imgFile: "patterns/natal.jpg",
-    font: "400 1rem/1 'Poppins', sans-serif",
-    color: "#ffffff",
-    quality: 0.8,
-  },
-  {
-    name: "Aniversário",
-    width: 1080,
-    height: 1080,
-    x: 100,
-    y: 100,
-    imgFile: "patterns/aniversario.jpg",
-    font: "400 3rem/1 'Poppins', sans-serif",
-    color: "#ffffff",
-    quality: 0.8,
-  },
-];
+//   name
+//   width
+//   height
+//   x
+//   y
+//   imgFile
+//   font
+//   color
+//   quality
